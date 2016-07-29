2fc17589f718a3863ff7d15e068759d68b838092
Assossa <AssossaGPB@users.noreply.github.com>
2016-07-29 18:12:19 -0400

    Update cache.lua

--- modules/cache.lua
+++ modules/cache.lua
@@ -1,23 +1,23 @@
 function load(path)
     if fs.exists(path) then
         local file = fs.open(path, "r")
-        _G.cache[path] = fs.readAll()
+        _G.icache[path] = fs.readAll()
         file.close()
     end
 end
 
 function saveFile(path)
-    if _G.cache[path] == nil then
+    if _G.icache[path] == nil then
         return
     end
 
     local file = fs.open(path, "w")
-    file.write(_G.cache[path])
+    file.write(_G.icache[path])
     file.close()
 end
 
 function save()
-    for k,_ in pairs(_G.cache) do
+    for k,_ in pairs(_G.icache) do
         if type(k) == "string" then
             saveFile(k)
         end
@@ -25,11 +25,11 @@ function save()
 end
 
 function refresh()
-    if _G.cache == nil then
-        _G.cache = {}
+    if _G.icache == nil then
+        _G.icache = {}
     end
 
-    for _,v in pairs(_G.cache) do
+    for _,v in pairs(_G.icache) do
         if type(v) == "string" then
             load(v)
         end
@@ -37,21 +37,21 @@ function refresh()
 end
 
 function write(path, data)
-    _G.cache[path] = data
+    _G.icache[path] = data
 end
 
 function append(path, data)
-    if _G.cache[path] ~= nil then
-        _G.cache[path] = _G.cache[path] .. data
+    if _G.icache[path] ~= nil then
+        _G.icache[path] = _G.icache[path] .. data
     else
         write(path, data)
     end
 end
 
 function read(path)
-    if _G.cache[path] == nil then
+    if _G.icache[path] == nil then
         load(path)
     end
 
-    return _G.cache[path]
+    return _G.icache[path]
 end
