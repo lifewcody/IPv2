b65470e2f2eca80ab19eca7f3c26b24363f8bb61
Assossa <AssossaGPB@users.noreply.github.com>
2016-07-29 17:56:13 -0400

    Create cache.lua

---
+++ modules/cache.lua
@@ -0,0 +1,53 @@
+function load(path)
+    if fs.exists(path) then
+        local file = fs.open(path, "r")
+        _G.cache[path] = fs.readAll()
+        file.close()
+    end
+end
+
+function saveFile(path)
+    if _G.cache[path] == nil then
+        return
+    end
+
+    local file = fs.open(path, "w")
+    file.write(_G.cache[path])
+    file.close()
+end
+
+function save()
+    for _,v in pairs(_G.cache) do
+        saveFile(v)
+    end
+end
+
+function refresh()
+    if _G.cache == nil then
+        _G.cache = {}
+    end
+
+    for _,v in pairs(_G.cache) do
+        load(v)
+    end
+end
+
+function write(path, data)
+    _G.cache[path] = data
+end
+
+function append(path, data)
+    if _G.cache[path] ~= nil then
+        _G.cache[path] = _G.cache[path] + data
+    else
+        write(path, data)
+    end
+end
+
+function read(path)
+    if _G.cache[path] == nil then
+        load(path)
+    end
+
+    return _G.cache[path]
+end
