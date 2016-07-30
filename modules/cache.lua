70ac4fee88994e7f98f11fcceca5b8945315010c
Assossa <AssossaGPB@users.noreply.github.com>
2016-07-30 13:47:11 -0400

    Update cache.lua

--- modules/cache.lua
+++ modules/cache.lua
@@ -1,69 +1,75 @@
-local cacheFile
+--[[
+cache.load(path) - Loads a file into the cache
+cache.saveFile(path) - Saves a specific file from the cache to the drive
+cache.save() - Saves all cached files to the drive
+cache.refresh() - Replaces cached files with version on drive, ignores cached files not on drive
+cache.write(path, data) - Writes data to path in cache, overwrites existing data
+cache.append(path, data) - Appends data to path in cache, includes existing data
+cache.read(path) - Returns data in path. If file not in cache, but on drive, then it is loaded into the cache and returned. Returns nil if file is not on cache or drive
+--]]
 
-function init(file)
-
-	cacheFile = file
-	
-	if fs.exists(file) == false then
-		cache = {}
-		writeCache()
-		return
-	end
-	
-	local cf = fs.open(file, "r")
-	_G["cache"] = textutils.unserialize(cf.readAll())
-	
-	if _G["cache"] == nil then
-		_G["cache"] = {}
-		writeCache()
-	end
-	
-	cf.close()
-	
+function load(path)
+    if fs.exists(path) then
+        _G.ucache[path] = false
+        local file = fs.open(path, "r")
+        _G.icache[path] = fs.readAll()
+        file.close()
+    end
 end
 
-local function isInitialized()
+function saveFile(path)
+    if _G.icache[path] == nil then
+        return
+    end
 
-	if cacheFile == nil or _G["cache"] == nil then
-		return false
-	else
-		return true
-	end
-
+    _G.ucache[path] = false
+    local file = fs.open(path, "w")
+    file.write(_G.icache[path])
+    file.close()
 end
 
-function writeCache()
-
-	if not isInitialized() then
-		return
-	end
-
-	local cf = fs.open(cacheFile, "w")
-	cf.write(textutils.serialize(_G["cache"]))
-	cf.close()
-
+function save()
+    for k,_ in pairs(_G.icache) do
+        if type(k) == "string" and _G.ucache[k] == true then
+            saveFile(k)
+        end
+    end
 end
 
-function lCache(cName)
+function refresh()
+    if _G.icache == nil then
+        _G.icache = {}
+    end
 
-	if not isInitialized() then
-		return
-	end
+    if _G.ucache == nil then
+        _G.ucache = {}
+    end
 
-	if _G["cache"][cName] == nil then
-		return false
-	else
-		return _G["cache"][cName]
-	end
-
+    for _,v in pairs(_G.icache) do
+        if type(v) == "string" then
+            load(v)
+        end
+    end
 end
 
-function wCache(cName, data)
-	
-	if not isInitialized() then
-		return
-	end
-	
-	_G["cache"][cName] = data
+function write(path, data)
+    _G.icache[path] = data
+    _G.ucache[path] = true
+end
 
+function append(path, data)
+    if _G.icache[path] ~= nil then
+        _G.icache[path] = _G.icache[path] .. data
+        _G.ucache[path] = true
+    else
+        write(path, data)
+    end
+end
+
+function read(path)
+    if _G.icache[path] == nil then
+        load(path)
+    end
+
+    return _G.icache[path]
 end
