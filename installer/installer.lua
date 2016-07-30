53562ee12290f5518318281b2278ec626ca7b63b
Assossa <yt.assossa@gmail.com>
2016-07-30 15:34:21 -0400

    Completed the installer

--- installer/installer.lua
+++ installer/installer.lua
@@ -4,12 +4,51 @@ local program
 function getPrograms()
     local p = http.get("https://raw.githubusercontent.com/InZernetTechnologies/IPv2/master/installer/programs.ccon")
     programs = textutils.unserialize(p.readAll())
+    p.close()
 end
 
 function printPrograms()
     if not programs then return end
     for i=1,#programs do
         print("[" .. i .. "] " .. programs[i][1])
+    end
+end
+
+function inputProgram()
+    term.write("[?] Select a program to install: ")
+    program = tonumber(read())
+    if not program or program <= 0 or program > #programs then
+        print("[!] Invalid program number")
+        inputProgram()
+    end
+end
+
+function downloadFile(url, path)
+    local u = http.get("https://raw.githubusercontent.com/InZernetTechnologies/IPv2/master/" .. url)
+    local f = fs.open(path, "w")
+    f.write(u.readAll())
+    f.close()
+    u.close()
+end
+
+function installProgram()
+    local i = programs[program][3]
+    local j = i .. "/" .. i
+    downloadFile(programs[program][2], j)
+    print("[*] Downloaded " .. j)
+end
+
+function installModules()
+    local i = http.get("https://raw.githubusercontent.com/InZernetTechnologies/IPv2/master/modules/module_list.ccon")
+    local mod = textutils.unserialize(i.readAll())
+    i.close()
+
+    for j=1,#mod do
+        local k = "modules/" .. mod[j][3]
+        if not fs.exists(k) then
+            downloadFile(mod[j][2], k)
+            print("[*] Downloaded " .. k)
+        end
     end
 end
 
@@ -25,10 +64,18 @@ function main()
 
     print("")
     printPrograms()
-    term.write("[?] Select a program to install: ")
-    program = tonumber(read())
+    print("")
 
-    print("You selected " .. program .. " which is the " .. programs[program][1])
+    inputProgram()
+
+    print("")
+    print("[*] Installing " .. programs[program][1] .. "...")
+
+    installProgram()
+    installModules()
+
+    print("")
+    print("[*] Successfully installed " .. programs[program][1])
 end
 
 main()
