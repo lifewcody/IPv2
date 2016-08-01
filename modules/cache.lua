5bd9791c9168803387e55f61ec81311f6f295cdf
Assossa <yt.assossa@gmail.com>
2016-08-01 12:58:07 -0400

    Simplified Cache Module

--- modules/cache.lua
+++ modules/cache.lua
@@ -1,42 +1,4 @@
---[[
-cache.load(path) - Loads a file into the cache
-cache.saveFile(path) - Saves a specific file from the cache to the drive
-cache.save() - Saves all cached files to the drive
-cache.refresh() - Replaces cached files with version on drive, ignores cached files not on drive
-cache.write(path, data) - Writes data to path in cache, overwrites existing data
-cache.append(path, data) - Appends data to path in cache, includes existing data
-cache.read(path) - Returns data in path. If file not in cache, but on drive, then it is loaded into the cache and returned. Returns nil if file is not on cache or drive
---]]
-
-function load(path)
-    if fs.exists(path) then
-        _G.ucache[path] = false
-        local file = fs.open(path, "r")
-        _G.icache[path] = fs.readAll()
-        file.close()
-    end
-end
-
-function saveFile(path)
-    if _G.icache[path] == nil then
-        return
-    end
-
-    _G.ucache[path] = false
-    local file = fs.open(path, "w")
-    file.write(_G.icache[path])
-    file.close()
-end
-
-function save()
-    for k,_ in pairs(_G.icache) do
-        if type(k) == "string" and _G.ucache[k] == true then
-            saveFile(k)
-        end
-    end
-end
-
-function refresh()
+local function check()
     if _G.icache == nil then
         _G.icache = {}
     end
@@ -44,29 +6,51 @@ function refresh()
     if _G.ucache == nil then
         _G.ucache = {}
     end
+end
 
-    for _,v in pairs(_G.icache) do
-        if type(v) == "string" then
-            load(v)
+local function saveFile(path)
+    ipath = _G.workingDir .. path
+
+    if _G.icache[path] == nil then
+        return
+    end
+
+    _G.ucache[path] = false
+    local file = fs.open(ipath, "w")
+    file.write(_G.icache[path])
+    file.close()
+end
+
+local function loadCache(path)
+    ipath = _G.workingDir .. path
+    if fs.exists(ipath) then
+        _G.ucache[path] = false
+        local file = fs.open(ipath, "r")
+        _G.icache[path] = fs.readAll()
+        file.close()
+    end
+end
+
+function setPath(path)
+    _G.workingDir = path
+end
+
+function saveCache()
+    for k,_ in pairs(_G.icache) do
+        if type(k) == "string" and _G.ucache[k] == true then
+            saveFile(k)
         end
     end
 end
 
-function write(path, data)
+function writeCache(path, data)
     _G.icache[path] = data
     _G.ucache[path] = true
 end
 
-function append(path, data)
-    if _G.icache[path] ~= nil then
-        _G.icache[path] = _G.icache[path] .. data
-        _G.ucache[path] = true
-    else
-        write(path, data)
-    end
-end
+function readCache(path)
+    check()
 
-function read(path)
     if _G.icache[path] == nil then
         load(path)
     end
