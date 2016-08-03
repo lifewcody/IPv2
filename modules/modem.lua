62e7a3939d7616e372e00f842dd2c27b68f30994
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-08-03 12:09:19 -0400

    ADDED Wireless Modem partial support
    ADDED - SSID, password, and channel selection

--- modules/modem.lua
+++ modules/modem.lua
@@ -1,3 +1,19 @@
+local WiFi = {}
+
+local function generatePassword(length)
+	local code
+	local validChars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for i = 1, length do
+		local idx = math.random(#validChars)
+		if code == nil then
+			code = validChars:sub(idx,idx)
+		else
+			code = code..validChars:sub(idx,idx)
+		end
+	end
+	return code
+end
+
 function openModems()
 
 	for a=1, #rs.getSides() do
@@ -5,12 +21,27 @@ function openModems()
 		if peripheral.getType(rs.getSides()[a]) == "modem" then
 		
 			if peripheral.call(rs.getSides()[a], "isWireless") then
-				print("Wireless modem on " .. rs.getSides()[a])
+				_G["log.lua"].log("DEBUG", "Found wirless modem on " .. rs.getSides()[a])
+				_G["log.lua"].log("DEBUG", "Selecting random channel for WiFi")
+				WiFi["channel"] = math.random(150, 175)
+				_G["log.lua"].log("DEBUG", "Selected channel " .. WiFi["channel"])
+				_G["log.lua"].log("DEBUG", "Opening WiFi")
+				WiFi["SSID"] = "CC" .. os.getComputerID() .. string.upper(string.sub(rs.getSides()[a],1,1))
+				WiFi["password"] = generatePassword(6)
+				for k,v in pairs(WiFi) do
+					print(k .. " > " .. v)
+				end
 			else
-				print("Wired modem on " .. rs.getSides()[a])
+				_G["log.lua"].log("DEBUG", "Found wired modem on " .. rs.getSides()[a])
 			end
 		end
 	
 	end
 	
+end
+
+function modemLoop()
+
+
+
 end
