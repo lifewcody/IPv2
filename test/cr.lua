e14ee2aa289666523b346b5f580e4f4d30ba4c61
Cody Robinson <cody-admin@inzernettechnologies.com>
2016-08-03 12:34:14 -0400

    UPDATED - To reflect modules
    Testing cache and modems modules

--- test/cr.lua
+++ test/cr.lua
@@ -1,27 +1,35 @@
-os.unloadAPI(shell.dir() .. "/modules/cache.lua")
-os.unloadAPI(shell.dir() .. "/modules/log.lua")
-os.unloadAPI(shell.dir() .. "/modules/update.lua")
+os.unloadAPI(shell.dir() .. "/../modules/cache.lua")
+os.unloadAPI(shell.dir() .. "/../modules/log.lua")
+os.unloadAPI(shell.dir() .. "/../modules/update.lua")
+os.unloadAPI(shell.dir() .. "/../modules/modem.lua")
 
-if os.loadAPI(shell.dir() .. "/modules/cache.lua") then
+if os.loadAPI(shell.dir() .. "/../modules/cache.lua") then
 	print("cache.lua loaded")
 end
-if os.loadAPI(shell.dir() .. "/modules/log.lua") then
+if os.loadAPI(shell.dir() .. "/../modules/log.lua") then
 	print("log.lua loaded")
 end
 
-if os.loadAPI(shell.dir() .. "/modules/update.lua") then
+if os.loadAPI(shell.dir() .. "/../modules/update.lua") then
 	print("log.lua loaded")
 end
 
-_G["cache.lua"].init(".cache")
+if os.loadAPI(shell.dir() .. "/../modules/modem.lua") then
+	print("modem.lua loaded")
+end
+
+--[[_G["cache.lua"].init(".cache")
 _G["cache.lua"].wCache("test", "nuff")
 _G["log.lua"].setLogLevel(6)
 _G["log.lua"].log("warn", "test")
 _G["cache.lua"].writeCache()
 
 _G["update.lua"].setType("router")
-_G["update.lua"].setURL("http://pastebin.com/raw/d9u0SceS")
 _G["update.lua"].setVersion("2016.7.29.02.07")
 _G["update.lua"].checkForUpdates()
 print("Needs updates? " .. tostring(_G["update.lua"].needsUpdates()))
+]]--
+_G["cache.lua"].setPath(shell.dir() .. "/")
+_G["modem.lua"].openModems()
+_G["cache.lua"].saveCache()
 
