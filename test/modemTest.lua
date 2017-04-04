685a3c3a770a2c8b2ef57d0194ff8dbacccff928
Cody Robinson <crobinson@inzernettechnologies.com>
2017-04-04 19:17:43 -0400

    Added modem test

---
+++ test/modemTest.lua
@@ -0,0 +1,7 @@
+if os.loadAPI(_G.iOS.dir .. "/../modules/module_manager.lua") == false then
+	error("[EMERG] module_manager error loading")
+end
+_G["module_manager.lua"].init()
+_G["module_manager.lua"].load()
+
+_G.modules.modem.openModems()
