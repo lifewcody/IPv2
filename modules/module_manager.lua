870b5bb2c77befdca3c24f7d8ddb021536969a38
Cody Robinson <crobinson@inzernettechnologies.com>
2017-04-04 03:50:32 -0400

    Fixed directory

--- modules/module_manager.lua
+++ modules/module_manager.lua
@@ -4,12 +4,13 @@ function load_module(path)
     end
 end
 
-function load()
-    local files = fs.list("/modules")
+function load(...)
+    local files = fs.list(_G.iOS.dir .. "/../modules")
     for i=1,#files do
         local j = files[i]
-        if j ~= "module_manager" then
-            load_module("/modules/" .. j)
+        if j ~= "module_manager.lua" then
+            load_module(_G.iOS.dir .. "/../modules/" .. j)
+			print("Attempting to load {" .. j .. "}")
         end
     end
 end
