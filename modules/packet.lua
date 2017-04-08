2d0305c069c4f321eba40212a5be2ed3a9d8e5e6
Cody Robinson <cody-admin@inzernettechnologies.com>
2017-04-08 06:40:12 -0400

    Added 2 new packet checking functions - IPCheck() and packetCheck()

--- modules/packet.lua
+++ modules/packet.lua
@@ -161,6 +161,31 @@ function setUDPData(data, udpData)
     return data
 end
 
+-- Packet Checking Functions
+
+-- Check to make sure that the IP is numbers and not ham.sandwich
+function IPCheck(ip)
+    local pIP = {} -- Create blank table
+    if ip == nil then return false end -- If it's nil, forget it
+    for octect in string.gmatch(ip, "[^.]+") do -- Find the . and put each side of it into a table
+        table.insert(pIP, octect) -- Insert here ;)
+    end -- Lua would be angry if we didn't end
+    if #pIP == 2 then -- Our IPs are 2 octets, not more, not less -- 2.
+        if tonumber(pIP[1]) and tonumber(pIP[2]) then return true end -- If they're both numbers, cool
+    end
+    return false -- If they're not, welp.
+end
+
+function packetCheck(packet)
+    if not #packet == 6 then return false end -- Must be a table 6 in length
+    if not (tonumber(packet[1]) == 2) then return false end -- Check IPv2
+    if tonumber(packet[2]) ~= 1 and packet[2] ~= 6 and packet[2] ~= 17 then return false end -- Check ICMP, UDP, or TCP
+    if not (tonumber(packet[3]) >= 1) then return false end -- TTL is greater or equal to 1
+    if not IPCheck(packet[4]) or not IPCheck(packet[5]) then return false end -- The packet (to/from) must be numbers
+    if packet[6] == nil then return false end -- Must contain some data
+    return true -- If our packet meets all the criteria
+end
+
 -- REQUIRED MODULE FUNCTIONS
 function getModuleInformation()
     return moduleInformation
