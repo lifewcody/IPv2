dfcfbc44861d334418791512c4e961a9c6a5e588
Cody Robinson <lifewcody@gmail.com>
2017-09-13 08:23:29 -0400

    FIXED - Issue modems not loading back correctly ADDED - Global modems instead of local FIXED - Statistics not being reset each time they are initialized/opened FIXED - Modems not being closed

--- libraries/modem.lua
+++ libraries/modem.lua
@@ -8,14 +8,14 @@ local log = require("IPv2/log")
     whereas connectedModemToLoad is the iteration
     of the function
 ]]
-local connectedModems = component.list("modem")
-local connectedModemToLoad = connectedModems()
+local connectedModems
+local connectedModemToLoad
 
 --[[
     Declare all of our variables, that are not
     a require
 ]]
-local modems = {}
+_G.modems = cache.get("modems")
 local emptyModem = {
     [ "RX" ] = 0,
     [ "TX" ] = 0,
@@ -25,34 +25,38 @@ local emptyModem = {
 }
 
 function modem.initialize()
-    modems = cache.get("modems")
+    connectedModems = component.list("modem")
+    connectedModemToLoad = connectedModems()
     while connectedModemToLoad ~= nil do
-        if modems[connectedModemToLoad] == nil then
+        if _G.modems[connectedModemToLoad] == nil then
             log.log("INFO", "FOUND", "MODEM", connectedModemToLoad)
-            modems[connectedModemToLoad] = emptyModem
+            _G.modems[connectedModemToLoad] = emptyModem
         end
         connectedModemToLoad = connectedModems()
     end
-    modem.open()
+    -- Memory Savings
+    connectedModemToLoad = nil
 end
 
-function modem.getStats()
-    return modems
+function modem.save()
+    cache.set("modems", _G.modems)
 end
 
 function modem.addRX(modemMAC, amountToAdd)
-    if modems[modemMAC] ~= nil then
-        local rx = tonumber(modems[modemMAC]["RX"])
-        rx = rx + tonumber(amountToAdd)
-        modems[modemMAC]["RX"] = rx
+    if _G.modems[modemMAC] ~= nil then
+        _G.modems[modemMAC]["RX"] = _G.modems[modemMAC]["RX"] + amountToAdd
     else
         log.log("NOTICE", "modem.addRX", "Modem does not exist")
     end
 end
 
 function modem.open()
-    for currentModem, modemProperties in pairs(modems) do
+    for currentModem, modemProperties in pairs(_G.modems) do
+        component.invoke(currentModem, "close")
         log.log("INFO", "LOADING", "MODEM", currentModem)
+        -- Reset statistics
+            modemProperties["RX"] = 0
+            modemProperties["TX"] = 0
         for _, VLAN in pairs(modemProperties["VLAN"]) do
             component.invoke(currentModem, "open", VLAN)
             log.log("INFO", "OPENING", "MODEM", VLAN)
