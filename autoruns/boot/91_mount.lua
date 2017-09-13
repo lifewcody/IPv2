6046929d3ac90b57635edff9c7c5bc0742ba5bdc
Cody Robinson <lifewcody@gmail.com>
2017-09-13 08:20:52 -0400

    ADDED - new RAID address

--- autoruns/boot/91_mount.lua
+++ autoruns/boot/91_mount.lua
@@ -9,5 +9,8 @@ for address, componentType in component.list("filesystem") do
     if address == "f0ce470e-4356-4a7b-986a-a3a41de2a064" then
         filesystem.mount("f0ce470e-4356-4a7b-986a-a3a41de2a064", "/IPv2")
     end
+    if address == "fe852d87-d709-4a60-8f1d-0d80c90925d6" then
+        filesystem.mount("fe852d87-d709-4a60-8f1d-0d80c90925d6", "/IPv2")
+    end
 
 end
